9b8f23ae866a1665ccf6a27cc3b58b7be26793bb
Adam Miller <amill242@uwo.ca>
2020-11-04 13:11:09 -0500

    Committing completed Date.h file -> this version has everything in the UML diagram, with one modification: the Date constructor is not (int,int,int) isntead of (String)

--- Date.h
+++ Date.h
@@ -1,4 +1,20 @@
+/*
+Author: Adam Miller
+Description: Header file that defines a date record, holding information on day, month, and year
+Date: 2020-11-04
+*/
 #ifndef DATE_H
 #define DATE_H
-class Date
+#include <string>
+class Date{
+private:    
+    int month, day, year; //month is an int from 1-12, day is an int from 1-31, year is an int>0
+public:
+    Date(int, int, int);
+    ~Date();
+    int getMonth();
+    int getDay();
+    int getYear();
+    std::string getStr();
+};
 #endif
