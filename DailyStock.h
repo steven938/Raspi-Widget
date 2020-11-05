0d5110335ea567258792b572933ebbe7b4783bab
Adam Miller <amill242@uwo.ca>
2020-11-05 13:49:27 -0500

    fixing typos

--- DailyStock.h
+++ DailyStock.h
@@ -1,8 +1,6 @@
 #ifndef DAILYSTOCK_H
 #define DAILYSTOCK_H
-
 #include "Date.h"
-
 class DailyStock{
 private:
 	float open; // open price on that date
@@ -12,12 +10,11 @@ private:
 	long long int volume; // volume of tradeson that date
 	Date date; // the date
 public:
-	DailyStock(float open, float close, float high, float low, long long int volume, string date);
+	DailyStock(float open, float close, float high, float low, long long int volume, std::string date);
 	float getOpen();
 	float getClose();
 	float getHigh();
 	float getLow();
 	long long int getVolume();
-}
-
+};
 #endif
