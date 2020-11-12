c8204683287bc98aa4b5360a3f4977e23832c529
Dhyey Patel <dpate256@uwo.ca>
2020-11-11 22:31:25 -0500

    newswindow comments

--- newswindow.cpp
+++ newswindow.cpp
@@ -1,6 +1,12 @@
 #include "newswindow.h"
 #include "ui_newswindow.h"
 
+/*
+Name: NewsWindow THIS ENTIRE CLASS NEEDS TO BE COMPLETED- JUST A TEMPLATE IS BUILD SO FAR
+Description: This is a constructor to create an instance of the NewsWindow class
+Parameter Descriptions: It needs a reference to the main window, and needs to get its widgets
+Return Description: N/A
+*/
 NewsWindow::NewsWindow(MainWindow * Window, QWidget *parent) :
     QWidget(parent),
     ui(new Ui::NewsWindow)
@@ -10,11 +16,24 @@ NewsWindow::NewsWindow(MainWindow * Window, QWidget *parent) :
 
 }
 
+/*
+Name: ~NewsWindow
+Description: This is a deconstructor to destroy the instance of the NewsWindow class
+Parameter Descriptions: N/A
+Return Description: N/A
+*/
 NewsWindow::~NewsWindow()
 {
+    // Delete the ui created in the constructor
     delete ui;
 }
 
+/*
+Name: on_BackButton_clicked
+Description: Method will execute when the back button is clicked
+Parameter Descriptions: N/A
+Return Description: N/A
+*/
 void NewsWindow::on_BackButton_clicked(){
     parentWindow->close();
     parentWindow = new MainWindow();
