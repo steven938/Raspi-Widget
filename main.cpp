c429ac8e13e4d3da1c1afdb35c218385c1e657fc
Adam Miller <amill242@uwo.ca>
2020-11-11 23:05:52 -0500

    commenting main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,25 @@
+/*
+Author: Adam Miller
+Description: CPP file for the main function
+Date: 2020-11-11
+*/
 #include "mainwindow.h"
 
 #include <QApplication>
 #include <QFontDatabase>
-
+/*
+Name: main
+Description: The main function, setups up a mainwindow. Most "driving" of the code is done by user input
+Parameter Descriptions: argc, argv unused
+Return Description: N/A
+*/
 int main(int argc, char *argv[])
 {
-    QFontDatabase::addApplicationFont(":/FreeSans.ttf");
-    QApplication a(argc, argv);
-    QFont font = QFont("FreeSans",10,1);
-    MainWindow w;
+    QFontDatabase::addApplicationFont(":/FreeSans.ttf");    //sets up the font for the app - required to ensure fonts display across platforms
+    QApplication a(argc, argv);                             //creates the qt framework that all UI exists within
+    QFont font = QFont("FreeSans",10,1);                    //sets up the font for the main window
+    MainWindow w;                                           //sets up the main window
     w.setFont(font);
-    w.showMaximized(); //hello
-    return a.exec();
+    w.show();                                               //shows the main window
+    return a.exec();                                        //required by qt
 }
